ac47c3120b54f969adeb10960a72a3972f402362
Andrei <galatyn@gmail.com>
2017-10-12 13:28:18 +0200

    +adot.Assert

---
+++ adot.Assert.pas
@@ -0,0 +1,35 @@
+unit adot.Assert;
+
+{$If Defined(DEBUG)}
+  {$Define HookAssert}
+{$EndIf}
+
+interface
+
+{$If Defined(HookAssert)}
+uses
+  Winapi.Windows;
+
+procedure Assert(Condition: Boolean; const Message: String = '');
+{$EndIf}
+
+implementation
+
+{$If Defined(HookAssert)}
+procedure Assert(Condition: Boolean; const Message: String = '');
+begin
+  {$If Defined(MSWindows)}
+    { At Windows platform we set breakpoint here automatically and then we continue
+      execution (to be able to inspect all conditions leading to the assertion) }
+    if not Condition then
+      DebugBreak;
+  {$Else}
+    { For non-Windows platform break point can be set manually here
+      to skip System.Assert and continue execution }
+    if not Condition then
+      System.Assert(Condition, Message);
+  {$EndIf}
+end;
+{$EndIf}
+
+end.
